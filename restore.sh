4467317bce0f3e5d6f70318c546f7bf38d222c81
Bernardo Salazar <bersalazar@gmail.com>
2021-01-23 10:40:49 +0100

    Add a restore script

---
+++ restore.sh
@@ -0,0 +1,19 @@
+echo 'Restoring all dot files'
+
+echo 'neovim config'
+cp ./nvim/* ~/.config/nvim/
+
+echo 'rectangle'
+cp ./rectangle/com.knollsoft.Rectangle.plist ~/Library/Preferences/
+
+echo 'zshrc'
+cp ./zsh/.zshrc ~/
+
+echo 'iterm2'
+cp ./iterm2/com.googlecode.iterm2.plist ~/Library/Preferences/
+
+echo 'powerlevel10k'
+cp ./powerlevel10k/.p10k.zsh ~/ 
+
+echo 'git global config'
+cp ./git/.gitconfig  ~/
