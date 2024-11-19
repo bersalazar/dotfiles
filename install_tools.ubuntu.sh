223ba0aa3d38c01969536148d023fcd18689064a
Bernardo Salazar <bersalazar@gmail.com>
2024-11-19 14:01:52 +0100

    yaml ls

--- install_tools.ubuntu.sh
+++ install_tools.ubuntu.sh
@@ -57,3 +57,6 @@ sudo apt install playerctl
 
 # go formatting
 go install -v github.com/incu6us/goimports-reviser/v3@latest
+
+# install yaml language server
+sudo snap install yaml-language-server
