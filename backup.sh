a61a14b6f59f1661798253e36bf002bbd4b0c282
Bernardo Salazar <bersalazar@gmail.com>
2025-10-20 07:57:37 +0200

    cleanup

--- backup.sh
+++ backup.sh
@@ -5,37 +5,34 @@ set -euo pipefail
 os="$(uname)"
 
 echo "Creating backup folders"
-mkdir -p {eslint,git,i3,iterm2,karabiner,logicprox,logind,nvim,powerlevel10k,rectangle,scripts,tmux,zsh}
+mkdir -p {dunst,git,i3,nvim,powerlevel10k,tmux,xserver,zsh}
 
 echo "Backing up dotfiles..."
 
 # OS-specific backup
 echo "This OS is ${os}. Backing up OS-specific files"
 if [[ "${os}" == "Linux" ]]; then
-    echo "zshrc"
-    cp ~/.zshrc ./zsh/.zshrc
-    
+    echo "dunst"
+    cp ~/.config/dunst/dunstrc ./dunst/
+
     echo "git global config"
     cp ~/.gitconfig ./git/
+
+    echo "i3"
+    cp ~/.i3/* ./i3/
 
     echo "powerlevel10k"
     cp ~/.p10k.zsh ./powerlevel10k/
     
     echo "tmux"
     cp ~/.tmux.conf ./tmux/
+
+    echo "xserver"
+    cp ~/.Xmodmap ./xserver/
     
-    echo "i3"
-    cp ~/.i3/* ./i3/
-
-    echo "dunst"
-    cp ~/.config/dunst/dunstrc ./dunst/
-
-elif [[ "${os}" == "Darwin" ]]; then
-    echo "rectangle"
-    cp ~/Library/Preferences/com.knollsoft.Rectangle.plist ./rectangle/
+    echo "zshrc"
+    cp ~/.zshrc ./zsh/.zshrc
 fi
-
-# dotfiles that apply to both linux and darwin
 
 echo "neovim config"
 cp -r ~/.config/nvim/* ./nvim/
